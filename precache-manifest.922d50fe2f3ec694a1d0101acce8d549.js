50d3bb358051baa06d89e23ffe3611965cd8b1cc
Robin Uitbeijerse <uitbeijerse@coffeeit.nl>
2020-12-02 11:09:02 +0100

    Updates

---
+++ precache-manifest.922d50fe2f3ec694a1d0101acce8d549.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae56384b2b8774d7c1d099936352cc0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2d3bfda335b4501ebf7d",
+    "url": "./static/css/main.caad5748.chunk.css"
+  },
+  {
+    "revision": "2be8d876fe0894751f54",
+    "url": "./static/js/2.00639bee.chunk.js"
+  },
+  {
+    "revision": "90565ba0b43e1b95d2876295dfd7014d",
+    "url": "./static/js/2.00639bee.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d3bfda335b4501ebf7d",
+    "url": "./static/js/main.219441e8.chunk.js"
+  },
+  {
+    "revision": "476224d929e535ab6f02",
+    "url": "./static/js/runtime-main.854ea85f.js"
+  }
+]);
